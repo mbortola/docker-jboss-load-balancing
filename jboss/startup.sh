c3ca64f8dd6fd155f72ecabea93ab60981e89374
mbortola <michele.bortol@gmail.com>
2016-04-28 22:26:28 +0200

    clustering finally fixed

--- jboss/startup.sh
+++ jboss/startup.sh
@@ -2,6 +2,9 @@
 
 $JAVA_HOME/bin/keytool -import -trustcacerts -file /etc/ssl/extras/apache.crt -alias ssl-web-server -noprompt -keystore $JAVA_HOME/jre/lib/security/cacerts -keypass changeit -storepass changeit
 
-echo "Node name: $NODE_NAME"
+export BIND_IP=`ifconfig | grep "inet " | grep broadcast | awk '{print $2}'`
 
-/home/jboss/EAP-6.4.0/jboss-eap-6.4/bin/standalone.sh  --server-config=standalone-ha.xml -Djboss.node.name=$NODE_NAME
+echo "Node name: $NODE_NAME: bind-address at $BIND_IP"
+
+# old/home/jboss/EAP-6.4.0/jboss-eap-6.4/bin/standalone.sh  --server-config=standalone-ha.xml -Djboss.node.name=$NODE_NAME
+/home/jboss/EAP-6.4.0/jboss-eap-6.4/bin/standalone.sh  --server-config=standalone-ha.xml -b $BIND_IP
---
+++ scripts/launcher.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+ openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout ../apache2/files/certs/apache.key -out ../apache2/files/certs/apache.crt -subj /CN=docker.domain.it
+
+ #docker build -t web-server ../apache2
+
+ #docker build -t jboss ../jboss
+
+ docker-compose up
