4570d1e9b2b88191446a7ed76fbf50c8a87d42cc
mbortola <michele.bortol@gmail.com>
2016-05-04 23:55:48 +0200

    apache sed fixing

--- apache2/files/startup.sh
+++ apache2/files/startup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #Works only in compose-mode
-#echo "Creating certificate.."
-#openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout /etc/ssl/extras/apache.key -out /etc/ssl/extras/apache.crt -subj /CN=$MY_DOMAIN
-#echo "..certificate created, some seds.."
+
 #JKLoads
-sed '7 a       JkMount /webapp loadbalancer' /opt/local-ssl.conf.orig >  /etc/apache2/sites-available/local-ssl.conf
+sed -i '7 a       JkMount /webapp loadbalancer' /opt/local-ssl.conf
 
 #seds
-sed -i 's|MY_DOMAIN|'"$MY_DOMAIN"'|g' /etc/apache2/sites-available/local-ssl.conf
+sed -i 's|MY_DOMAIN|'"$MY_DOMAIN"'|g' /opt/local-ssl.conf
+
+mv /opt/local-ssl.conf /etc/apache2/sites-available/
 
 echo "127.0.0.1  $MY_DOMAIN" >> /etc/hosts
 
