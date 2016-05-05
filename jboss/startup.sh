3ce59bc292773afc818b5556ead0413d646604f8
Bortolato Michele <michele.bortolato@infocert.it>
2016-05-05 17:17:37 +0200

    jboss configuration improved

--- jboss/startup.sh
+++ jboss/startup.sh
@@ -2,9 +2,14 @@
 
 $JAVA_HOME/bin/keytool -import -trustcacerts -file /etc/ssl/extras/apache.crt -alias ssl-web-server -noprompt -keystore $JAVA_HOME/jre/lib/security/cacerts -keypass changeit -storepass changeit
 
+# retrieve binding ip
 export BIND_IP=`ifconfig | grep "inet " | grep broadcast | awk '{print $2}'`
 
 echo "Node name: $NODE_NAME: bind-address at $BIND_IP"
 
-# old/home/jboss/EAP-6.4.0/jboss-eap-6.4/bin/standalone.sh  --server-config=standalone-ha.xml -Djboss.node.name=$NODE_NAME
-/home/jboss/EAP-6.4.0/jboss-eap-6.4/bin/standalone.sh  --server-config=standalone-ha.xml -b $BIND_IP
+# Start the jboss, run configuration script and tail server.log
+/home/jboss/EAP-6.4.0/jboss-eap-6.4/bin/standalone.sh  --server-config=standalone-ha.xml -b $BIND_IP &
+
+/home/jboss/EAP-6.4.0/jboss-eap-6.4/bin/jboss-cli -c --file=/opt/configure.txt
+
+fg
--- scripts/launcher.sh
+++ scripts/launcher.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
- openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout ../apache2/files/certs/apache.key -out ../apache2/files/certs/apache.crt -subj /CN=docker.domain.it
+ openssl req -x509 -nodes -days 3650 -newkey rsa:2048 -keyout ../apache2/files/certs/apache.key -out ../apache2/files/certs/apache.crt -subj /CN=identity.local.infocert.it
 
  #docker build -t web-server ../apache2
 
